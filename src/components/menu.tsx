ad607b761713f8baa108fec9c9a0264184ce605a
Sebastián Antonio Cano Mena <99150630+SebastianCanoMena@users.noreply.github.com>
2023-06-07 15:46:53 -0600

    changed colors and modified menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,14 +1,19 @@
 export default function Menu()
 {
 	return(
-		<div className='container m-0 bg-info h-100'>
-			<div className='row p-2'>
-				<ul className='col'>
-					<li>History</li>
-					<li>Account</li>
-					<li>Settings</li>
-				</ul>
+		<div className='container m-0 h-100 menu'>
+			<div className='row p-2 d-flex flex-column text-center'>
+				<div className='col menu-col'>
+					History
+				</div>
+				<div className='col menu-col'>
+					Account
+				</div>
+				<div className='col menu-col'>
+					Settings
+				</div>
 			</div>	
 		</div>
 	)
-}
+}					
+					
